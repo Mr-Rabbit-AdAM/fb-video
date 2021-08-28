b5d4ffa280635a196a5d77895187a930b37f2479
Ad-AM <87221134+BCKS-AdAM@users.noreply.github.com>
2021-08-29 02:39:23 +0630

    Create fb.sh

---
+++ fb.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+clear 
+
+echo ""
+
+echo -e "\e[5;32m   
+
+
+⢸⣿⣿⣿⣿⠃⠄⢀⣴⡾⠃⠄⠄⠄⠄⠄⠈⠺⠟⠛⠛⠛⠛⠻⢿⣿⣿⣿⣿⣶⣤⡀⠄
+⢸⣿⣿⣿⡟⢀⣴⣿⡿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣸⣿⣿⣿⣿⣿⣿⣿⣷
+⢸⣿⣿⠟⣴⣿⡿⡟⡼⢹⣷⢲⡶⣖⣾⣶⢄⠄⠄⠄⠄⠄⢀⣼⣿⢿⣿⣿⣿⣿⣿⣿⣿
+⢸⣿⢫⣾⣿⡟⣾⡸⢠⡿⢳⡿⠍⣼⣿⢏⣿⣷⢄⡀⠄⢠⣾⢻⣿⣸⣿⣿⣿⣿⣿⣿⣿
+⡿⣡⣿⣿⡟⡼⡁⠁⣰⠂⡾⠉⢨⣿⠃⣿⡿⠍⣾⣟⢤⣿⢇⣿⢇⣿⣿⢿⣿⣿⣿⣿⣿
+⣱⣿⣿⡟⡐⣰⣧⡷⣿⣴⣧⣤⣼⣯⢸⡿⠁⣰⠟⢀⣼⠏⣲⠏⢸⣿⡟⣿⣿⣿⣿⣿⣿
+⣿⣿⡟⠁⠄⠟⣁⠄⢡⣿⣿⣿⣿⣿⣿⣦⣼⢟⢀⡼⠃⡹⠃⡀⢸⡿⢸⣿⣿⣿⣿⣿⡟
+⣿⣿⠃⠄⢀⣾⠋⠓⢰⣿⣿⣿⣿⣿⣿⠿⣿⣿⣾⣅⢔⣕⡇⡇⡼⢁⣿⣿⣿⣿⣿⣿⢣
+⣿⡟⠄⠄⣾⣇⠷⣢⣿⣿⣿⣿⣿⣿⣿⣭⣀⡈⠙⢿⣿⣿⡇⡧⢁⣾⣿⣿⣿⣿⣿⢏⣾
+⣿⡇⠄⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢻⠇⠄⠄⢿⣿⡇⢡⣾⣿⣿⣿⣿⣿⣏⣼⣿
+⣿⣷⢰⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⢰⣧⣀⡄⢀⠘⡿⣰⣿⣿⣿⣿⣿⣿⠟⣼⣿⣿
+⢹⣿⢸⣿⣿⠟⠻⢿⣿⣿⣿⣿⣿⣿⣿⣶⣭⣉⣤⣿⢈⣼⣿⣿⣿⣿⣿⣿⠏⣾⣹⣿⣿
+⢸⠇⡜⣿⡟⠄⠄⠄⠈⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟⣱⣻⣿⣿⣿⣿⣿⠟⠁⢳⠃⣿⣿⣿
+⠄⣰⡗⠹⣿⣄⠄⠄⠄⢀⣿⣿⣿⣿⣿⣿⠟⣅⣥⣿⣿⣿⣿⠿⠋⠄⠄⣾⡌⢠⣿⡿⠃
+⠜⠋⢠⣷⢻⣿⣿⣶⣾⣿⣿⣿⣿⠿⣛⣥⣾⣿⠿⠟⠛⠉⠄⠄
+
+
+
+"
+
+echo -e "\e[1;31m  _______ Code By Ad A M _______
+"
+
+echo "1. Termux  "
+
+echo ""
+echo "2. Linux"
+echo ""
+read -p "Enter Choose ::  " s 
+
+if [[ $s == 1 ]];then 
+termux-setup-storage 
+mkdir /sdcard/fb-video
+pkg install python -y
+pip install --upgrade pip 
+pip install fb-down 
+clear 
+echo -e "\e[1;32m  Code By  Ad A M "
+
+echo ""
+
+read -p "Enter the Video Url Link :: " url 
+echo ""
+read -p "Enter the you want to give :: " s 
+echo "" 
+echo -e "\e[1;32m××××××××××× start ×××××××××××"
+fbdown $url --output /sdcard/fb-video/$s.mp4 
+sleep 2 
+clear 
+echo -e "\e[1;32m____________ Done ___________"
+
+elif [[ $s == 2 ]];then
+sudo pip install fb-down
+mkdir fb-video
+clear 
+echo -e "\e[1;32m  Code By  Ad A M "
+
+echo ""
+
+read -p "Enter the Video Url Link :: " url 
+echo ""
+read -p "Enter the you want to give ::  " s 
+echo "" 
+echo -e "\e[1;32m××××××××××× start ×××××××××××"
+fbdown $url --output fb-video/$s.mp4 | grep url
+sleep 2 
+clear 
+echo -e "\e[1;32m____________ Done ____________"
+fi
